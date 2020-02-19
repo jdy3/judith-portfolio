9b8fda1f4d205f6560d780ff436db87db65dbee0
Judith <jyouziel20@hotmail.com>
2020-02-19 15:37:24 +0000

    update portfolio with project links and pics

--- index.js
+++ index.js
@@ -1,16 +1,125 @@
-let textP = document.getElementById('textP');
+let textP = document.getElementById("textP");
 
-let heart = document.getElementById('heart');
+let heart = document.getElementById("heart");
 
-let arrow = document.getElementById('arrow');
+let arrow = document.getElementById("arrow");
 
-let instruct = document.getElementById('instruct')
+let instruct = document.getElementById("instruct");
 
 function anim(event) {
-  heart.className = heart.className.replace('beat', 'enable');
-  textP.className = textP.className.replace('hide', 'reveal');
-  arrow.classList.add('hide');
-  instruct.classList.add('hide');
+  heart.className = heart.className.replace("beat", "enable");
+  textP.className = textP.className.replace("hide", "reveal");
+  arrow.classList.add("hide");
+  instruct.classList.add("hide");
 }
 
-heart.addEventListener('click', anim);
+heart.addEventListener("click", anim);
+
+/*start of image carousel*/
+var slides = document.getElementsByClassName("pose");
+var dots = document.getElementsByClassName("dot");
+var playPauseButton = document.getElementById("playPause");
+
+(function() {
+  slides[0].style.display = "block";
+  dots[0].className += " active";
+  playPauseButton.innerHTML = "&#9658;";
+  playPauseButton.classList.add("start");
+})();
+
+var slideIndex = 1;
+var slideInterval = setInterval(slideShow, 2000);
+var playing = false;
+
+function slideShow() {
+  var i;
+
+  if (playing) {
+    playPauseButton.className = playPauseButton.className.replace("start", "");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slideIndex++;
+
+    if (slideIndex > (slides.length += 1)) {
+      slideIndex = 2;
+    }
+
+    slides[slideIndex - 2].style.display = "block";
+    dots[slideIndex - 2].className += " active";
+  }
+}
+
+function pauseSlideShow() {
+  playing = false;
+  clearInterval(slideInterval);
+  playPauseButton.innerHTML = "&#9658;";
+}
+
+function playSlideShow() {
+  playing = true;
+  slideInterval = setInterval(slideShow, 2000);
+  playPauseButton.innerHTML = "&#9616;&nbsp;&#9612";
+}
+
+function togglePlay() {
+  if (playing) {
+    pauseSlideShow();
+  } else {
+    playSlideShow();
+  }
+}
+
+function showSlides(n) {
+  playing = false;
+  clearInterval(slideInterval);
+  playPauseButton.className = playPauseButton.className.replace("start", "");
+  var i;
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  playPauseButton.innerHTML = "&#9658;";
+}
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function leftArrowPressed() {
+  showSlides((slideIndex -= 1));
+}
+
+function rightArrowPressed() {
+  showSlides((slideIndex += 1));
+}
+
+document.onkeydown = function(event) {
+  switch (event.keyCode) {
+    case 37:
+      leftArrowPressed();
+      break;
+    case 39:
+      rightArrowPressed();
+      break;
+  }
+};
+/*end of image carousel*/
